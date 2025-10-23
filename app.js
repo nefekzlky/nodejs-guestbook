70c96d605c5be3ce2472102919e5154140ce9746
Necmettin Efe <kizilkayanecmettinefe@gmail.com>
2025-10-23 23:47:31 +0300

    refactor: Move initial route to routes/guestbook.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const guestbookRoutes = require('./routes/guestbook');
+
 const PORT = 3000;
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/',(req,res) => {
+app.use('/',guestbookRoutes);
 
-    res.render('guestbook');
 
-});
 
 app.listen(PORT,() => {
     console.log(`The server running on http://localhost:${PORT}`);
